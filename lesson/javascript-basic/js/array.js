5f44166e15f07b6c800ce422e1970aa347d59a69
tayoshi-tokisada <mail.by.blacksheep@gmail.com>
2024-04-10 17:10:10 +0900

    change lesson ~kadai_008

---
+++ lesson/javascript-basic/js/array.js
@@ -0,0 +1,16 @@
+// 配列の宣言と値の代入を行う
+const userNames = ['侍太郎', '侍一郎', '侍二郎', '侍三郎', '侍四郎'];
+
+// 配列の値を出力する
+console.log(userNames);
+
+// 2番目の要素を更新する
+userNames[1] = '侍花子';
+
+// 6番目の要素を追加する
+userNames[5] = '侍五郎';
+
+// 配列の値を出力する
+console.log(userNames);
+
+console.log(userNames[2]);
